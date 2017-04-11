5a64094c3074de7351ff7d45935d477879b76a8b
hatwell <caroline.hatwell@gmail.com>
2017-04-11 15:40:11 +0100

    added some methods to photographer

--- Photographer.java
+++ Photographer.java
@@ -3,7 +3,19 @@ import java.util.*;
 public class Photographer {
 
   private String name;
-  private ArrayList<Camera> camera;
+  private ArrayList<Camera> cameras;
 
-  
+  public Photographer(String name){
+    this.name = name;
+    this.cameras = new ArrayList<Camera>();
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void addCamera(Camera camera){
+    this.cameras.add(camera);
+  }
+
 }
