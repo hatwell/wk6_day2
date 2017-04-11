8d1dada06aeab8bea59c8be5c5fe612341e934ca
hatwell <caroline.hatwell@gmail.com>
2017-04-11 15:17:03 +0100

    created test classes and bare bones of other classes

---
+++ PhotographerTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class PhotographerTest {
+  Photographer photographer = new Photographer();
+  DigitalCamera digitalCamera = new DigitalCamera();
+  AnalogueCamera analogueCamera = new AnalogueCamera();
+
+}
